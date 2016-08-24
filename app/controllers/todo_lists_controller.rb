d9dc11b5ffd619d8af6e620bcb8d1e7501917e47
Youfar <wakafar@gmail.com>
2016-08-24 11:08:12 +0900

    first commit

---
+++ app/controllers/todo_lists_controller.rb
@@ -0,0 +1,46 @@
+class TodoListsController < ApplicationController
+
+  def index
+  	@todo_lists = TodoList.all
+  end
+
+  def create
+    @todo_list = TodoList.new(todo_list_params)
+ 
+    @todo_list.save
+    redirect_to @todo_list
+  end
+
+  def update
+    @todo_list = TodoList.find(params[:id])
+ 
+    if @todo_list.update(todo_list_params)
+      redirect_to @todo_list
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+  	@todo_list = TodoList.find(params[:id])
+  	@todo_list.destroy
+
+  	redirect_to todo_lists_path
+  end
+
+  def edit
+  	@todo_list = TodoList.find(params[:id])
+  end
+
+  def show
+  	@todo_list = TodoList.find(params[:id])
+  end
+
+  private
+
+  def todo_list_params
+    params.require(:todo_list).permit(:title)
+  end
+
+
+end
